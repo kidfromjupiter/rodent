c5733a22ce7981e41b5f4c2985ea995cab499bab
Lasan Mahaliyana <limahaliyana@gmail.com>
2026-04-02 18:22:35 +0530

    removed build artifacts

--- build/CMakeFiles/refresh_bluez_codegen.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/refresh_bluez_codegen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/refresh_bluez_codegen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
